c49be252d781baa9e705517887358c095f66b1a1
yaroslav <reznikovaroslav419@gmail.com>
2023-07-12 14:06:38 +0300

    begining of my first java project

---
+++ src/MyVoolfrineAlpha/AlphaMain.java
@@ -0,0 +1,6 @@
+package MyVoolfrineAlpha;
+public class AlphaMain {
+	public static void main(String [] args) {
+		System.out.print("Hello World");
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author yaroslav
+ *
+ */
+module MyVoolfrineAlpha {
+}
